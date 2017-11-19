9363f0ab81e6b137318972a55f146fb654a910ea
vinicius <vns.vinisouza@gmail.com>
2017-11-19 14:14:19 -0200

    teste

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,7 +2,8 @@ module.exports = function(app){
 
 //tratativa para requisição o express passou a ter este metodo que é nativo do EJS
 app.get('/', function(req, res){
-//local onde o EJS vai pegar o arquivo
+
+	//local onde o EJS vai pegar o arquivo
  	res.render('home/index');
 });
 }
